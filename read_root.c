22b07a8a6aac3b1a5eec5358f345d1849539d133
nicolas-palacio <69064260+nicolas-palacio@users.noreply.github.com>
2023-09-25 20:48:32 -0300

    Se completaron los atributos del Fat12BootSector

--- read_root.c
+++ read_root.c
@@ -10,12 +10,25 @@ typedef struct {
     char file_size[4];
 } __attribute((packed)) PartitionTable;
 
-typedef struct {
-    unsigned char jmp[3];
-    char oem[8];
-    unsigned short sector_size;
-	// {...} COMPLETAR
-    unsigned int volume_id;
+typedef struct { //Unsigned = hexa
+    unsigned char jmp[3]; //Bytes 0-2
+    char oem[8]; //Bytes 3-10
+    unsigned short sector_size; //Bytes 11-12 Tam del sector, en nuestro caso 512 bytes. short = 2bytes. char= 1byte . long=4bytes.
+	unsigned char sectors_per_cluster; //Byte 13
+    unsigned short reserved_sectors; //Bytes 14-15, en nuestro caso 1.
+    unsigned char number_of_fats; //Byte 16.Cant de FATs, en nuestro caso 2.
+    unsigned short root_dir_entries; //Byte 17-18. Cant maxima de entradas de dir, en nuestro caso 512.
+    unsigned short root_num_sectors; //Bytes 19-20.
+    unsigned char media_type; //Byte 21.
+    unsigned short fat_size_sectors; //Bytes 22-23, en nuestro caso 2 sectores= 2*512bytes=1024bytes.
+    unsigned short sectors_per_track;//Bytes 24-25.
+    unsigned short number_of_heads; //Bytes 26-27
+    unsigned long number_of_sectors; //Bytes 28-31.
+    unsigned long number_of_sectors_fs; //Bytes 32-35.
+    unsigned char bios_int; //Byte 36.
+    unsigned char not_used; //Byte 37.
+    unsigned char ext_boot_signaturel //Byte 38.
+    unsigned int volume_id;//Bytes 39-42.
     char volume_label[11];
     char fs_type[8];
     char boot_code[448];
